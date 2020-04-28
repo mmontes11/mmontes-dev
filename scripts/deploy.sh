a143e20978b781b78f3627230b2081b0fbd02572
Martín Montes <martin11lrx@gmail.com>
2020-04-28 16:21:50 +0000

    Updated manifests and deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,3 @@
 set -e
 
 kubectl apply -f manifests --recursive
-
-namespaces=("kubernetes-dashboard" "mmontes-dev")
-for ns in "${namespaces[@]}"; do
-    echo "☸️   $ns resources:"
-    kubectl get all -o wide -n="$ns"
-done
