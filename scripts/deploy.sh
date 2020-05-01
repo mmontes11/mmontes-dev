df7d0989470fdfb6338d16dadec79d40b48b15fc
Martín Montes <martin11lrx@gmail.com>
2020-05-01 15:22:08 +0000

    Added mongodb exporter and its dashboard

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,7 @@ set -e
 kubectl apply -f manifests/kubernetes-dashboard
 
 kubectl apply -f manifests/monitoring --recursive
+helm install mongodb-exporter stable/prometheus-mongodb-exporter -f config/helm/mongodb-exporter.values.yml -n monitoring
 helm install prometheus stable/prometheus -f config/helm/prometheus.values.yml -n monitoring
 helm install grafana stable/grafana -f config/helm/grafana.values.yml -n monitoring
 
