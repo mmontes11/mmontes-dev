0c6724317bde1edeb2877b2778c4081a7fe5814f
Martín Montes <martin11lrx@gmail.com>
2021-02-20 13:56:43 +0100

    feat: support fold old stable charts and upgraded Grafana

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,12 @@ kubectl apply -f duckdns
 echo "🚀    Deploying kubernetes-dashboard ..."
 kubectl apply -f kubernetes-dashboard
 
-echo "🚀    Deploying monitoring ..."
+echo "🚀    Deploying monitoring manifests ..."
 kubectl apply -f monitoring/manifests --recursive
-for i in monitoring/*; do
+
+echo "🚀    Deploying monitoring stable charts ..."
+helm repo add stable https://charts.helm.sh/stable --force-update
+for i in monitoring/stable-charts/*; do
     if [ -d "$i" ]; then
         continue
     fi
@@ -21,7 +24,12 @@ for i in monitoring/*; do
     helm upgrade "$name" "stable/$name" -f "$values" -n monitoring --install
 done
 
-echo "🚀    Deploying traefik ..."
+echo "🚀    Deploying Grafana ..."
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+helm upgrade grafana grafana/grafana -f grafana.values.yml -n monitoring --install
+
+echo "🚀    Deploying Traefik ..."
 kubectl apply -f traefik/manifests
 helm upgrade traefik stable/traefik -f traefik/values.yml -n kube-system --install
 kubectl delete ingress traefik-dashboard
