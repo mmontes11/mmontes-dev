bf02cf6630aa4f84f30ba0ec09c241fc4529202b
Martín Montes <martin11lrx@gmail.com>
2021-03-14 17:10:04 +0100

    feat: migrated to kube prometheus stack

--- deploy.sh
+++ deploy.sh
@@ -2,35 +2,25 @@
 
 set -e
 
-echo "🚀    Deploying duckdns ..."
+echo "☸️  Adding helm repos ..."
+helm repo add traefik https://helm.traefik.io/traefik
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+echo "🚀 Deploying duckdns ..."
 kubectl apply -f duckdns
 
-echo "🚀    Deploying Traefik ..."
-helm repo add traefik https://helm.traefik.io/traefik
-helm upgrade traefik traefik/traefik -f traefik/values.yml --install
-kubectl apply -f traefik/manifests
+echo "🚀 Deploying monitoring ..."
+kubectl apply -f monitoring/manifests/
 
-echo "🚀    Deploying monitoring ..."
-kubectl apply -f monitoring/manifests/ -R
-
-echo "🚀    Deploying kubernetes-dashboard ..."
-helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+echo "🚀 Deploying kubernetes-dashboard ..."
 helm upgrade kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard -n monitoring --install
 
-echo "🚀    Deploying monitoring stable charts ..."
-helm repo add stable https://charts.helm.sh/stable --force-update
-for i in monitoring/stable-charts/*; do
-    if [ -d "$i" ]; then
-        continue
-    fi
-    values="$i"
-    name="$(basename -- $i)"
-    name="${name%.*.*}"
-    echo "$name $values"
-    helm upgrade "$name" "stable/$name" -f "$values" -n monitoring --install
-done
+echo "🚀 Deploying kube-prometheus-stack ..."
+helm upgrade kube-prometheus-stack prometheus-community/kube-prometheus-stack \
+  -f monitoring/kube-prometheus-stack.values.yml -n monitoring --install
 
-echo "🚀    Deploying Grafana ..."
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo update
-helm upgrade grafana grafana/grafana -f monitoring/grafana.values.yml -n monitoring --install
+echo "🚀 Deploying traefik ..."
+helm upgrade traefik traefik/traefik -f traefik/values.yml --install
+kubectl apply -f traefik/manifests
